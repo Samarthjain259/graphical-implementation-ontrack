be153ef95cf7042791343ba18b014a656d3a837e
Guest <imangulardev@gmail.com>
2019-11-21 02:23:18 +0530

    first commit

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -5,7 +5,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
 import { NotificationsComponent } from '../../notifications/notifications.component';
 import { ChartsModule } from 'ng2-charts';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -23,7 +22,6 @@ import { ToastrModule } from 'ngx-toastr';
   declarations: [
     DashboardComponent,
     UserProfileComponent,
-    TableListComponent,
     NotificationsComponent,
   ]
 })
